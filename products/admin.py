6dc5d84d57265614a15212a26614065a5105cfed
2macs <>
2023-06-03 12:33:19 +0000

    created catgorys in admin panle, added products for thriller category

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-
-from . import Product
-
-# Register your models here.
-from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
