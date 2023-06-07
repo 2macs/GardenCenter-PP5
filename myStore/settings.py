8ed7d057827ce4bfb21cd1cd7bd88a7758386c6f
2macs <>
2023-06-07 19:46:24 +0000

    add toast message templates for success, error, info and warning

--- myStore/settings.py
+++ myStore/settings.py
@@ -87,6 +87,9 @@ TEMPLATES = [
     },
 ]
 
+# use toast with gitpod etc.
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     "django.contrib.auth.backends.ModelBackend",
