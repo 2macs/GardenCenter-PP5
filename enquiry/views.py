e7cb03aceb79a21186efadbb9ef6ca78c6961060
2macs <>
2023-06-07 20:53:27 +0000

    added toast messages to new enquiry app

--- enquiry/views.py
+++ enquiry/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
+
 from .forms import EnquiryForm
+
 
 # Create your views here.
 # Create your views here.
@@ -11,6 +14,9 @@ def make_enquiry(request):
         form = EnquiryForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.info(
+                request, f"Your enquiry has been registered, we will respond asap"
+            )
     else:
         form = EnquiryForm()
     return render(request, "enquiry/enquiry.html/", {"form": form})
