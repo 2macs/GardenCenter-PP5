c34e8455336aac6bceaeaef0f4251eaf7daa256e
2macs <>
2023-06-09 12:25:24 +0000

    started adding stripe element to app, getting stripe load error

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = "checkout/checkout.html"
     context = {
         "order_form": order_form,
+        "stripe_public_key": "pk_test_51Mzh6nK0F9yAasNPCBArpbuga6LTECX18E4nSeOeB6WWNJhzs8LMIO3aGxh05LKAvgMMEGtdLpaQAmkahrcVjTWk00Q0eanRkr",
+        "client_secret": "sk_test_51Mzh6nK0F9yAasNP481YBBKypenfQmWg0QvKmAYCzbfFBkPAr9S0fty4E2VAeadPjsN0RYAZi6DmZGa5pctOUgGo00Qd6CRkqA",
     }
 
     return render(request, template, context)
