c32b0561c2204972b53e2f5b6359cb65fe3cae58
2macs <>
2023-06-10 10:29:15 +0000

    removed upload_to from imagefield of product model as it was creating an extra media folder

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,45 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get("bag", {})
-    # prevents direct access from the page top bar
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse("products"))
 
+    current_bag = bag_contents(request)
+    total = current_bag["grand_total"]
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(
+            request,
+            "Stripe public key is missing. \
+            Did you forget to set it in your environment?",
+        )
+
     template = "checkout/checkout.html"
     context = {
         "order_form": order_form,
-        "stripe_public_key": "pk_test_51Mzh6nK0F9yAasNPCBArpbuga6LTECX18E4nSeOeB6WWNJhzs8LMIO3aGxh05LKAvgMMEGtdLpaQAmkahrcVjTWk00Q0eanRkr",
-        "client_secret": "sk_test_51Mzh6nK0F9yAasNP481YBBKypenfQmWg0QvKmAYCzbfFBkPAr9S0fty4E2VAeadPjsN0RYAZi6DmZGa5pctOUgGo00Qd6CRkqA",
+        "stripe_public_key": stripe_public_key,
+        "client_secret": intent.client_secret,
     }
 
     return render(request, template, context)
