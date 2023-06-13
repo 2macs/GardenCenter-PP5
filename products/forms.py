9ea67d9bdf27c04cbd11cf47ff17acb895e3e5f1
2macs <>
2023-06-13 15:55:10 +0000

    improve styling on imagefield in add products form

--- products/forms.py
+++ products/forms.py
@@ -1,17 +1,20 @@
 from django import forms
 from .models import Product, Category
+from .widgets import CustomClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        # dunder which includes all the model fields
         fields = "__all__"
+
+    image = forms.ImageField(
+        label="Image", required=False, widget=CustomClearableFileInput
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        # list comprehension , for loop shorthand
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields["category"].choices = friendly_names
