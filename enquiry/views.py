3aa9665e1b15272fa171d196657ef69b58309ae5
2macs <>
2023-06-13 19:57:09 +0000

    changed styling on enquire.html, enquiry posts are working

--- enquiry/views.py
+++ enquiry/views.py
@@ -4,11 +4,6 @@ from django.contrib import messages
 from .forms import EnquiryForm
 
 
-# Create your views here.
-# Create your views here.
-# Templates are Index, booking,enquire,explore
-
-
 def make_enquiry(request):
     if request.method == "POST":
         form = EnquiryForm(request.POST)
