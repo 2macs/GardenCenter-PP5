6e51f43d51d9abb0918ce101287172f2a2a8e662
2macs <>
2023-06-15 11:29:02 +0000

    created view, url and template for all reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import SiteReview
 
 # Create your views here.
+
+# View all site reviews
+
+def all_reviews(request):
+    reviews = SiteReview.objects.all()
+    return render(request, 'reviews/reviews.html', {'reviews': reviews})
