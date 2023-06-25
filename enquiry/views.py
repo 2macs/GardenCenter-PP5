90d5cac814d192add3529daa1a8e662747c4f866
2macs <>
2023-06-25 20:05:58 +0000

    email in enquiry timing out

--- enquiry/views.py
+++ enquiry/views.py
@@ -10,13 +10,22 @@ def make_enquiry(request):
     if request.method == "POST":
         form = EnquiryForm(request.POST)
         if form.is_valid():
+            cd = form.cleaned_data
             form.save()
             messages.info(
                 request, f"Your enquiry has been registered, we will respond asap"
             )
             subject = f"Thank you for your enquiry"
-            body = f"Dear {form['name']} , we will respond asap"
-            send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [form["email"]])
+            body = f"Dear {cd['name']} , we will respond asap"
+            send_mail(
+                subject,
+                body,
+                "amcneill83@gmail.com",
+                [
+                    cd["email"],
+                ],
+                fail_silently=False,
+            )
     else:
         form = EnquiryForm()
     return render(request, "enquiry/enquiry.html/", {"form": form})
